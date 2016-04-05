28d7158ae5ff0956baebf5bb4e7d73ed955a230e
Godfrey Chiu <ggwc82@gmail.com>
2016-04-05 16:00:31 +0100

    Extracted sulphuras and increase_quality private methods

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -8,23 +8,17 @@ class GildedRose
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          sulfuras(item)
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              increase_quality(item)
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              increase_quality(item)
             end
           end
         end
@@ -36,21 +30,33 @@ class GildedRose
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+              sulfuras(item)
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          increase_quality(item)
         end
       end
     end
   end
+
+  private
+
+
+  def sulfuras(item)
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.quality = item.quality - 1
+    end
+  end
+
+  def increase_quality(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+    end
+  end
+
 end
 
 class Item
